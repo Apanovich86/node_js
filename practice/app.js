f435abfd633d637a83ac74c7dbc329d111fa1231
Apanovich86 <apanovich.anna.itec@gmail.com>
2021-10-03 11:59:54 +0300

    dz, practice node_js

--- practice/app.js
+++ practice/app.js
@@ -1,9 +1,11 @@
 const fs = require('fs');
 const path = require('path');
+
 const mkdirPath1 = path.join(__dirname, 'manOlder20');
 const mkdirPath2 = path.join(__dirname, 'manYounger20');
 const mkdirPath3 = path.join(__dirname, 'womanOlder20');
 const mkdirPath4 = path.join(__dirname, 'womanYounger20');
+
 const users = [
     {name: 'Olya', gender: 'female', age: 35},
     {name: 'Daria', gender: 'female', age: 19},
@@ -14,37 +16,55 @@ const users = [
     {name: 'Svitlana', gender: 'female', age: 16},
     {name: 'Oleg', gender: 'male', age: 21}
 ];
-fs.mkdir(mkdirPath1, (e) => {
-    console.log(e);
-});
-fs.mkdir(mkdirPath2, (e) => {
-    console.log(e);
-});
-fs.mkdir(mkdirPath3, (e) => {
-    console.log(e);
-});
-fs.mkdir(mkdirPath4, (e) => {
-    console.log(e);
-});
-const createFile = () => {
-    for (let i = 0; i < users.length; i++) {
+
+function createFolders() {
+    fs.mkdir(mkdirPath1, (err) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        fs.mkdir(mkdirPath2, (err) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            fs.mkdir(mkdirPath3, (err) => {
+                if (err) {
+                    console.log(err);
+                    return;
+                }
+                fs.mkdir(mkdirPath4, (err) => {
+                    if (err) {
+                        console.log(err);
+                        return;
+                    }
+                    createFiles(users);
+                })
+            })
+        })
+    })
+}
+
+function createFiles(users) {
+    users.forEach(user => {
         const fileContents = (dirPath) => {
-            fs.writeFile(`${dirPath}/${users[i].name}.txt`, `{name:${users[i].name}, gender:${users[i].gender}, age:${users[i].age}}`, (err) => {
+            fs.writeFile(`${dirPath}/${user.name}.txt`, `{name:${user.name}, gender:${user.gender}, age:${user.age}}`, (err) => {
                 console.log(err);
             })
         }
-        if (users[i].age > 20 && users[i].gender === "female") {
+        if (user.age > 20 && user.gender === "female") {
             fileContents(mkdirPath3);
         }
-        if (users[i].age < 20 && users[i].gender === "female") {
+        if (user.age < 20 && user.gender === "female") {
             fileContents(mkdirPath4);
         }
-        if (users[i].age > 20 && users[i].gender === "male") {
+        if (user.age > 20 && user.gender === "male") {
             fileContents(mkdirPath1);
         }
-        if (users[i].age < 20 && users[i].gender === "male") {
+        if (user.age < 20 && user.gender === "male") {
             fileContents(mkdirPath2);
         }
-    }
-};
-createFile();
+    })
+}
+
+createFolders();
